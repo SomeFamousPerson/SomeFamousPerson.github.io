c1c46e39c39b53989b927e5b0adb5e20aafa9f93
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2021-11-02 09:38:08 +1300

    Update index.js

--- login/index.js
+++ login/index.js
@@ -1,1 +1,5 @@
-
+let username = test;
+let password = test;
+// that moment when you realise im storing usernames and passowrds in plain text in an open-source repository
+let loginBox = document.createElement("input");
+// ok this is the extent of my JavaScript knowledge, someone finish this and make it good
